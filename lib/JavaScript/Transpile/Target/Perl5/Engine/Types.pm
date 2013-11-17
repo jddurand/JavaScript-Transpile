affe9d27eaa7498175e4f209a23e21ec0eca9088
Jean-Damien Durand <jeandamiendurand@free.fr>
2013-11-17 23:40:15 +0100

    Handle JavaScript UTF16 asymetry v.s. perl utf8

--- lib/JavaScript/Transpile/Target/Perl5/Engine/Types.pm
+++ lib/JavaScript/Transpile/Target/Perl5/Engine/Types.pm
@@ -16,6 +16,7 @@ Math::BigInt->config(
 	trap_nan => 0,         # No croak on NaN
 	round_mode => 'even'   # Rounds to the nearest value, except for 5 which is equidistant, in which case it rounds to the nearest even digit.
     });
+use Encode qw/encode decode from_to/;
 
 =head1 DESCRIPTION
 
@@ -62,11 +63,35 @@ subtype 'Number',    as 'Num';
 #                         FileHandle
 #                         Object                     <=> Undefined
 
-use constant trueAsString      => [ unpack('S*', 'true') ];
-use constant falseAsString     => [ unpack('S*', 'false') ];
-use constant undefinedAsString => [ unpack('S*', 'undefined') ];
-use constant nullAsString      => [ unpack('S*', 'null') ];
-use constant referenceAsString => [ unpack('S*', 'Reference') ];
+#
+# Take care: String is a sequence of UTF-16 Code Units, NOT characters.
+# This mean that internally JavaScript is using UCS-2, not UTF-16. I.e.
+# no support for surrogate pairs. Nevertheless the spec says that textual
+# data is supposed to be UTF-16. I.e. the input can contain surrogate pairs.
+# Surrogate pairs will not be taken as they are, but as new characters,
+# because of the UCS-2 internal encoding.
+#
+# To get the unsigned short values out of a UTF-16LE: unpack('v*', ...).
+# To get the unsigned short values out of a UTF-16BE: unpack('n*', ...).
+#
+sub utf8ToArrayOfUnsignedShort {
+    return [ unpack('v*', encode("UTF-16LE", $_[0])) ];
+}
+
+sub arrayOfUnsignedShortToUtf8 {   # Note: this is NOT symmetric
+    my $rc = '';
+    map {$rc .= chr($_)} @{$_[0]};
+    return $rc;
+}
+
+use constant trueAsString      => utf8ToArrayOfUnsignedShort('true');
+use constant falseAsString     => utf8ToArrayOfUnsignedShort('false');
+use constant undefinedAsString => utf8ToArrayOfUnsignedShort('undefined');
+use constant nullAsString      => utf8ToArrayOfUnsignedShort('null');
+use constant referenceAsString => utf8ToArrayOfUnsignedShort('Reference');
+use constant ZeroStringValue   => utf8ToArrayOfUnsignedShort('0');
+use constant xStringValue      => utf8ToArrayOfUnsignedShort('x');
+use constant XStringValue      => utf8ToArrayOfUnsignedShort('X');
 
 # Undefined is mapped to single value unknown
 coerce 'Undefined', from 'Any', via { unknown };
@@ -83,14 +108,24 @@ coerce 'Number',
     from 'Null',      via { Math::BigInt->new(undef) };  # 0
     from 'Reference', via { Math::BigInt->new("$_") };
     from 'Undefined', via { Math::BigInt->new(unknown) };  # NaN
-    from 'String',    via { ($_ eq 'Infinity') ? Math::BigInt->binf() : Math::BigInt->new("$_") };
+    from 'String',    via { my $s = arrayOfUnsignedShortToUtf8($_);
+                            if ($s eq 'Infinity')  {
+				return Math::BigInt->binf();
+			    } elsif (length($s) >= 2 &&
+				     substr($s, 0, 1) eq '0' &&
+				     (substr($s, 1, 1) ne 'x' && substr($s, 0, 1) ne 'X')) {
+				return Math::BigInt->from_oct("$_");
+			    } else {
+				return Math::BigInt->new("$_");
+			    }
+};
 
 # String coercions
 coerce 'String',
     from 'Boolean',   via { $_ ? trueAsString : falseAsString };
     from 'Null',      via { nullAsString };
-    from 'Number',    via { [ unpack('S*', "$_") ] };
-    from 'Reference', via { [ unpack('S*', ref($_)) ] };
+    from 'Number',    via { utf8ToArrayOfUnsignedShort("$_") };
+    from 'Reference', via { utf8ToArrayOfUnsignedShort(ref($_)) };
     from 'Undefined', via { undefinedAsString };
 
 =head1 SEE ALSO
