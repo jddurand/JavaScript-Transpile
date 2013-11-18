324843a74af28f5948097ce0d3c4af28294e7079
Jean-Damien Durand <jeandamiendurand@free.fr>
2013-11-18 12:33:51 +0100

    Target/Perl5/Engine/Types: Oups, should be Math::BigFloat and not Math::BigInt

--- lib/JavaScript/Transpile/Target/Perl5/Engine/Types.pm
+++ lib/JavaScript/Transpile/Target/Perl5/Engine/Types.pm
@@ -8,15 +8,11 @@ package JavaScript::Transpile::Target::Perl5::Engine::Types;
 # VERSION
 
 use Unknown::Values;
-use Math::BigInt;
+use Math::BigFloat;
 use Moose::Util::TypeConstraints;
-Math::BigInt->config(
-    {
-	trap_inf => 0,         # No croak in inf
-	trap_nan => 0,         # No croak on NaN
-	round_mode => 'even'   # Rounds to the nearest value, except for 5 which is equidistant, in which case it rounds to the nearest even digit.
-    });
 use Encode qw/encode/;
+
+Math::BigFloat->round_mode('even');  # Just to be sure
 
 =head1 DESCRIPTION
 
@@ -91,6 +87,11 @@ use constant referenceAsString => utf8ToArrayOfUnsignedShort('Reference');
 use constant ZeroStringValue   => utf8ToArrayOfUnsignedShort('0');
 use constant xStringValue      => utf8ToArrayOfUnsignedShort('x');
 use constant XStringValue      => utf8ToArrayOfUnsignedShort('X');
+use constant NaNStringValue    => utf8ToArrayOfUnsignedShort('NaN');
+use constant InfPStringValue   => utf8ToArrayOfUnsignedShort('+Infinity');
+use constant InfMStringValue   => utf8ToArrayOfUnsignedShort('-Infinity');
+use constant ZeroPStringValue  => utf8ToArrayOfUnsignedShort('+0');
+use constant ZeroMStringValue  => utf8ToArrayOfUnsignedShort('-0');
 
 # Undefined is mapped to single value unknown
 coerce 'Undefined', from 'Any', via { unknown };
@@ -102,28 +103,64 @@ coerce 'Null', from 'Any', via { undef };
 coerce 'Boolean', from 'Any', via { $_ ? 1 : 0 };
 
 # Number is IEEE-754
+# In ECMAScript-262-5, Number special values are:
+# NaN
+# +Infinity
+#  Infinity
+# -Infinity
+# +0
+#  0
+# -0
 coerce 'Number',
-    from 'Boolean',   via { Math::BigInt->new($_ ? '1' : '0') };
-    from 'Null',      via { Math::BigInt->new(undef) };  # 0
-    from 'Reference', via { Math::BigInt->new("$_") };
-    from 'Undefined', via { Math::BigInt->new(unknown) };  # NaN
+    from 'Boolean',   via { Math::BigFloat->new($_ ? 1 : 0) };
+    from 'Null',      via { Math::BigFloat->new(undef) };  # 0
+    from 'Reference', via { Math::BigFloat->new("$_") };
+    from 'Undefined', via { Math::BigFloat->new(unknown) };  # NaN
     from 'String',    via { my $s = arrayOfUnsignedShortToUtf8($_);
-                            if ($s eq 'Infinity')  {
-				return Math::BigInt->binf();
+                            if ($s eq 'NaN') {
+                              return Math::BigFloat->bnan();
+                            } elsif ($s eq '+Infinity' || $s eq 'Infinity')  {
+                              return Math::BigFloat->binf();
+                            } elsif ($s eq '-Infinity')  {
+                              return Math::BigFloat->binf('-');
+                            } elsif ($s eq '+0' || $s eq '0')  {
+                              return Math::BigFloat->bzero();
+                            } elsif ($s eq '-0')  {
+                              return Math::BigFloat->new('-0');   # I believe this fallbacks to bzero()
 			    } elsif (length($s) >= 2 &&
 				     substr($s, 0, 1) eq '0' &&
 				     (substr($s, 1, 1) ne 'x' && substr($s, 0, 1) ne 'X')) {
-				return Math::BigInt->from_oct("$_");
+                              substr($s, 0, 1, '');
+                              return Math::BigFloat->new(oct($s));
 			    } else {
-				return Math::BigInt->new("$_");
+                              return Math::BigFloat->new("$s");
 			    }
 };
 
 # String coercions
 coerce 'String',
-    from 'Boolean',   via { $_ ? trueAsString : falseAsString };
-    from 'Null',      via { nullAsString };
-    from 'Number',    via { utf8ToArrayOfUnsignedShort("$_") };
+  from 'Boolean',   via { $_ ? trueAsString : falseAsString };
+  from 'Null',      via { nullAsString };
+  from 'Number',    via {
+    if ($_->is_nan()) {
+      return NaNStringValue;
+    } elsif ($_->is_inf()) {
+      return InfPStringValue;
+    } elsif ($_->is_inf('-')) {
+      return InfMStringValue;
+    } elsif ($_->is_zero()) {
+      #
+      # For the principle, I believe this is is_pos() always returns true...
+      #
+      if ($_->is_pos()) {
+        return ZeroPStringValue;
+      } else {
+        return ZeroMStringValue;
+      }
+    } else {
+      return utf8ToArrayOfUnsignedShort("$_")
+    }
+  };
     from 'Reference', via { utf8ToArrayOfUnsignedShort(ref($_)) };
     from 'Undefined', via { undefinedAsString };
 
