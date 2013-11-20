7efecc077500ccb7911ea4cd6c9e0cccb20f0d24
Jean-Damien Durand <jeandamiendurand@free.fr>
2013-11-20 21:16:02 +0100

    Refurbished Fdlibm.pm

--- lib/JavaScript/Transpile/Fdlib.pm
+++ lib/JavaScript/Transpile/Fdlib.pm
@@ -1,8 +1,13 @@
+use strict;
+use warnings FATAL => 'all';
+
 package JavaScript::Transpile::Fdlib;
 
+# ABSTRACT: fdlibm interface
+
+# VERSION
+
 use 5.010000;
-use strict;
-use warnings;
 use Carp;
 
 require Exporter;
@@ -138,8 +143,6 @@ our @EXPORT = qw(
 	_FDLIBM_XOPEN_
 );
 
-our $VERSION = '0.01';
-
 sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
     # XS function.
@@ -170,24 +173,17 @@ XSLoader::load('JavaScript::Transpile::Fdlib', $VERSION);
 
 # Autoload methods go after =cut, and are processed by the autosplit program.
 
-1;
-__END__
-# Below is stub documentation for your module. You'd better edit it!
-
 =head1 NAME
 
-JavaScript::Transpile::Fdlib - Perl extension for blah blah blah
+JavaScript::Transpile::Fdlib - Perl extension for fdlibm
 
 =head1 SYNOPSIS
 
   use JavaScript::Transpile::Fdlib;
-  blah blah blah
 
 =head1 DESCRIPTION
 
-Stub documentation for JavaScript::Transpile::Fdlib, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
+fdlibm, version 53, interface.
 
 Blah blah blah.
 
@@ -304,26 +300,8 @@ None by default.
 
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
-
-=head1 AUTHOR
-
-Jean-Damien Durand, E<lt>jdurand@E<gt>
-
-=head1 COPYRIGHT AND LICENSE
-
-Copyright (C) 2013 by Jean-Damien Durand
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.18.1 or,
-at your option, any later version of Perl 5 you may have available.
-
+L<http://www.netlib.org/fdlibm/>
 
 =cut
+
+1;
