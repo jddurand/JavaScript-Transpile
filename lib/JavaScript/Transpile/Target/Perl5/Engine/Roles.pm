86da294460b5dc6e11b3181e0c6b90ebc45fc877
Jean-Damien Durand <jeandamiendurand@free.fr>
2013-12-03 07:14:18 +0100

    Perl5/Engine/Roles.pm; typo fix dev+

--- lib/JavaScript/Transpile/Target/Perl5/Engine/Roles.pm
+++ lib/JavaScript/Transpile/Target/Perl5/Engine/Roles.pm
@@ -4,6 +4,7 @@ use warnings FATAL => 'all';
 package JavaScript::Transpile::Target::Perl5::Engine::Roles;
 
 use Log::Any qw/$log/;
+use JavaScript::Transpile::Target::Perl5::Engine::Roles::NamedAccessorProperty;
 use JavaScript::Transpile::Target::Perl5::Engine::Types;
 
 # ABSTRACT: JavaScript Object Type in Perl5
@@ -16,7 +17,7 @@ This module provides JavaScript Object type definition in a Perl5 environment.
 
 =cut
 
-has 'NamedDataPropery' => {
+has 'NamedDataProperty' => {
     isa => 'DataProperty'
 };
 
